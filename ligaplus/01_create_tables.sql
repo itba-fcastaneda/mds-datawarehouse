e6b2c25bcae8694234fbe354728f37ea5e0f25de
Federico Castañeda <fedecasta@gmail.com>
2023-12-14 08:26:28 -0300

    Update script

--- ligaplus/01_create_tables.sql
+++ ligaplus/01_create_tables.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS public."Subscription"
 (
     "Id" integer NOT NULL,
     "Type" character(40) COLLATE pg_catalog."default" NOT NULL,
-    "Description" character(80) COLLATE pg_catalog."default",
+    "Description" character(300) COLLATE pg_catalog."default",
     CONSTRAINT "Subscription_pkey" PRIMARY KEY ("Id")
 );
 
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS public."Content"
 
 CREATE TABLE IF NOT EXISTS public."Time"
 (
-    "Id" integer NOT NULL,
+    "Id" date NOT NULL,
     "Day" integer NOT NULL,
     "Month" integer NOT NULL,
     "Year" integer NOT NULL,
@@ -93,7 +93,7 @@ CREATE TABLE IF NOT EXISTS public."UserSearch"
 (
     "Id" integer NOT NULL,
     "UserId" integer NOT NULL,
-    "TimeId" integer NOT NULL,
+    "TimeId" date NOT NULL,
     "Query" character(128) COLLATE pg_catalog."default",
     CONSTRAINT "UserSearch_pkey" PRIMARY KEY ("Id")
 );
@@ -102,7 +102,7 @@ CREATE TABLE IF NOT EXISTS public."UserRecommendation"
 (
     "Id" integer NOT NULL,
     "UserId" integer NOT NULL,
-    "TimeId" integer NOT NULL,
+    "TimeId" date NOT NULL,
     "ContentId" integer NOT NULL,
     "PositionId" integer NOT NULL,
     CONSTRAINT "UserRecommendation_pkey" PRIMARY KEY ("Id")
@@ -112,7 +112,7 @@ CREATE TABLE IF NOT EXISTS public."ContentRating"
 (
     "ContentId" integer NOT NULL,
     "UserId" integer NOT NULL,
-    "TimeId" integer NOT NULL,
+    "TimeId" date NOT NULL,
     "Rating" integer NOT NULL,
     PRIMARY KEY ( "ContentId", "UserId")
 );
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS public."ContentUsage"
 (
     "ContentId" integer NOT NULL,
     "UserId" integer NOT NULL,
-    "TimeId" integer NOT NULL,
+    "TimeId" date NOT NULL,
     "Seconds" integer NOT NULL,
     PRIMARY KEY ( "ContentId", "UserId",  "TimeId")
 );
@@ -130,7 +130,7 @@ CREATE TABLE IF NOT EXISTS public."ContentHit"
 (
     "ContentId" integer NOT NULL,
     "UserId" integer NOT NULL,
-    "TimeId" integer NOT NULL,
+    "TimeId" date NOT NULL,
     "SearchId" integer NOT NULL,
     "RecommendationId" integer NOT NULL,
     PRIMARY KEY ( "ContentId", "UserId",  "TimeId")
@@ -159,3 +159,122 @@ ALTER TABLE IF EXISTS public."ContentHit" OWNER to postgres;
 
 
 
+ALTER TABLE IF EXISTS public."User"
+	ADD CONSTRAINT "fk_location"
+    FOREIGN KEY("LocationId") 
+	REFERENCES "Location"("Id");
+
+ALTER TABLE IF EXISTS public."User"
+	ADD CONSTRAINT "fk_segments"
+    FOREIGN KEY("SegmentId") 
+	REFERENCES "Segment"("Id");
+
+ALTER TABLE IF EXISTS public."UserSubscription"
+	ADD CONSTRAINT "fk_user"
+    FOREIGN KEY("UserId") 
+	REFERENCES "User"("Id");
+
+ALTER TABLE IF EXISTS public."UserSubscription"
+    ADD CONSTRAINT "fk_subscription"
+    FOREIGN KEY("SubscriptionId") 
+    REFERENCES "Subscription"("Id");
+
+ALTER TABLE IF EXISTS public."Content"
+    ADD CONSTRAINT "fk_category"
+    FOREIGN KEY("CategoryId") 
+    REFERENCES "Category"("Id");
+
+ALTER TABLE IF EXISTS public."SearchResponse"
+    ADD CONSTRAINT "fk_search"
+    FOREIGN KEY("SearchId") 
+    REFERENCES "UserSearch"("Id");
+
+ALTER TABLE IF EXISTS public."SearchResponse"
+    ADD CONSTRAINT "fk_content"
+    FOREIGN KEY("ContentId") 
+    REFERENCES "Content"("Id");
+
+ALTER TABLE IF EXISTS public."UserSearch"
+    ADD CONSTRAINT "fk_user_search"
+    FOREIGN KEY("UserId") 
+    REFERENCES "User"("Id");
+
+ALTER TABLE IF EXISTS public."UserSearch"
+    ADD CONSTRAINT "fk_time_search"
+    FOREIGN KEY("TimeId") 
+    REFERENCES "Time"("Id");
+
+ALTER TABLE IF EXISTS public."UserRecommendation"
+    ADD CONSTRAINT "fk_user_recommendation"
+    FOREIGN KEY("UserId") 
+    REFERENCES "User"("Id");
+
+ALTER TABLE IF EXISTS public."UserRecommendation"
+    ADD CONSTRAINT "fk_time_recommendation"
+    FOREIGN KEY("TimeId") 
+    REFERENCES "Time"("Id");
+
+ALTER TABLE IF EXISTS public."UserRecommendation"
+    ADD CONSTRAINT "fk_content_recommendation"
+    FOREIGN KEY("ContentId") 
+    REFERENCES "Content"("Id");
+
+ALTER TABLE IF EXISTS public."UserRecommendation"
+    ADD CONSTRAINT "fk_position_recommendation"
+    FOREIGN KEY("PositionId") 
+    REFERENCES "Position"("Id");
+
+ALTER TABLE IF EXISTS public."ContentRating"
+    ADD CONSTRAINT "fk_user_rating"
+    FOREIGN KEY("UserId") 
+    REFERENCES "User"("Id");
+
+ALTER TABLE IF EXISTS public."ContentRating"
+    ADD CONSTRAINT "fk_time_rating"
+    FOREIGN KEY("TimeId") 
+    REFERENCES "Time"("Id");
+
+ALTER TABLE IF EXISTS public."ContentRating"
+    ADD CONSTRAINT "fk_content_rating"
+    FOREIGN KEY("ContentId") 
+    REFERENCES "Content"("Id");
+
+ALTER TABLE IF EXISTS public."ContentUsage"
+    ADD CONSTRAINT "fk_user_usage"
+    FOREIGN KEY("UserId") 
+    REFERENCES "User"("Id");
+
+ALTER TABLE IF EXISTS public."ContentUsage"
+    ADD CONSTRAINT "fk_time_usage"
+    FOREIGN KEY("TimeId") 
+    REFERENCES "Time"("Id");
+
+ALTER TABLE IF EXISTS public."ContentUsage"
+    ADD CONSTRAINT "fk_content_usage"
+    FOREIGN KEY("ContentId") 
+    REFERENCES "Content"("Id");
+
+ALTER TABLE IF EXISTS public."ContentHit"
+    ADD CONSTRAINT "fk_user_hit"
+    FOREIGN KEY("UserId") 
+    REFERENCES "User"("Id");
+
+ALTER TABLE IF EXISTS public."ContentHit"
+    ADD CONSTRAINT "fk_time_hit"
+    FOREIGN KEY("TimeId") 
+    REFERENCES "Time"("Id");
+
+ALTER TABLE IF EXISTS public."ContentHit"
+    ADD CONSTRAINT "fk_content_hit"
+    FOREIGN KEY("ContentId") 
+    REFERENCES "Content"("Id");
+
+ALTER TABLE IF EXISTS public."ContentHit"
+    ADD CONSTRAINT "fk_search_hit"
+    FOREIGN KEY("SearchId") 
+    REFERENCES "UserSearch"("Id");
+
+ALTER TABLE IF EXISTS public."ContentHit"
+    ADD CONSTRAINT "fk_recommendation_hit"
+    FOREIGN KEY("RecommendationId") 
+    REFERENCES "UserRecommendation"("Id");
---
+++ ligaplus/10_insert_dimensions.sql
@@ -0,0 +1,67 @@
+
+--- Location
+INSERT INTO public."Location" ("Id", "State", "Country", "Lat", "Long" ) VALUES
+(1, 'Andalusia', 'Spain', 37.3890924, -5.9844589),
+(2, 'Aragon', 'Spain', 41.5976277, -0.9056621),
+(3, 'Asturias', 'Spain', 43.3613951, -5.8593269),
+(4, 'Balearic Islands', 'Spain', 39.5696005, 2.6501603),
+(5, 'Basque Country', 'Spain', 43.2630136, -2.9351482),
+(6, 'Canary Islands', 'Spain', 28.2915637, -16.6291304),
+(7, 'Cantabria', 'Spain', 43.1828396, -3.9878424),
+(8, 'Castile and León', 'Spain', 41.6822327, -4.4421391),
+(9, 'Castilla-La Mancha', 'Spain', 39.2795606, -3.097702),
+(10, 'Catalonia', 'Spain', 41.5911589, 1.5208624),
+(11, 'Extremadura', 'Spain', 39.4747689, -6.3729635),
+(12, 'Galicia', 'Spain', 42.5750554, -8.1338554),
+(13, 'La Rioja', 'Spain', 42.2870733, -2.5396036),
+(14, 'Madrid', 'Spain', 40.4167047, -3.7035825),
+(15, 'Murcia', 'Spain', 37.9923795, -1.1305431),
+(16, 'Navarre', 'Spain', 42.6953919, -1.6760691),
+(17, 'Valencian Community', 'Spain', 39.4699075, -0.3762881);
+
+--- Position
+INSERT INTO public."Position" ("Id", "Position")
+VALUES 
+(1, 'Home'),
+(2, 'Menu'),
+(3, 'Search Box'),
+(4, 'Carrousel'),
+(5, 'Dropdown');
+
+
+-- Insert data for Id 1
+INSERT INTO public."Subscription" ("Id", "Type", "Description")
+VALUES 
+(1, 'Basic', 'Este es el plan minimo que un usuario con subscripcion paga puede acceder, el contenido que se ofrece es menor a los planes siguientes'),
+(2, 'Standard', 'Este plan posee el contenido basico, mas una serie de eventos especiales dentro de los deportes nacionales mas vistos, como Petanca'),
+(3, 'Premium', 'Este es el plan mas caro de la plataforma, posee todo el contenido disponible a la fecha, sin limites');
+
+INSERT INTO public."Category" ("Id", "Description")
+VALUES
+(1, 'Futbol'),
+(2, 'Baloncesto'),
+(3, 'Tenis'),
+(4, 'Golf'),
+(5, 'Ciclismo'),
+(6, 'Automovilismo'),
+(7, 'Running'),
+(8, 'Voleibol'),
+(9, 'Balonmano'),
+(10, 'Petanca'),
+(11, 'Hockey'),
+(12, 'Motociclismo'),
+(13, 'Gimnasia'),
+(14, 'eSports');
+
+--- Create table time from 1900-01-01 to 2099-12-31
+INSERT INTO public."Time" ("Id", "Day", "Month", "Year")
+SELECT
+    date_sequence AS "Id",
+    EXTRACT(DAY FROM date_sequence) AS "Day",
+    EXTRACT(MONTH FROM date_sequence) AS "Month",
+    EXTRACT(YEAR FROM date_sequence) AS "Year"
+FROM (
+    SELECT generate_series(
+        '1900-01-01'::date, '2099-12-31'::date, interval '1 day'
+    )::date AS date_sequence
+) AS date_series;
